6f58430fc82e40c58e89aee835e6f8307ea831fe
Ahoaha Aaaach <awilliamson1889@gmail.com>
2021-08-17 10:18:09 +0300

    Update test REST for new logic.

--- department_app/tests/rest/test_address_new.py
+++ department_app/tests/rest/test_address_new.py
@@ -8,7 +8,7 @@ from department_app.tests import AddressFactory
 class TestApiAddress(unittest.TestCase):
     """Test address api class"""
 
-    @patch('department_app.rest.address.CRUDAddress.get_address')
+    @patch('department_app.rest.address.CRUDAddress.get')
     def test_get_address(self, mock_get_address):
         """Api should return information about address."""
         mock_get_address.return_value = {'id': 100,
@@ -19,9 +19,13 @@ class TestApiAddress(unittest.TestCase):
         self.assertEqual(response.json['id'], 100)
         self.assertEqual(response.json['name'], 'Some Address')
 
-    @patch('department_app.rest.address.CRUDAddress.update_address')
-    def test_put_address(self, mock_update_address):
+    @patch('department_app.rest.address.Address.check_exist')
+    @patch('department_app.rest.address.Address.get_json')
+    @patch('department_app.rest.address.CRUDAddress.update')
+    def test_put_address(self, mock_update_address, mok_get_json, mock_check_exist):
         """Api should return information about address."""
+        mok_get_json.return_value = {'name': 'Updated Address'}
+        mock_check_exist.return_value = True
         mock_update_address.return_value = {'id': 100,
                                             'name': 'Updated Address'}
         response = Address.put(100)
@@ -30,9 +34,11 @@ class TestApiAddress(unittest.TestCase):
         self.assertEqual(response.json['id'], 100)
         self.assertEqual(response.json['name'], 'Updated Address')
 
-    @patch('department_app.rest.address.CRUDAddress.create_address')
-    def test_post_address(self, mock_create_address):
+    @patch('department_app.rest.address.Address.get_json')
+    @patch('department_app.rest.address.CRUDAddress.create')
+    def test_post_address(self, mock_create_address, mok_get_json):
         """Api should return information about address."""
+        mok_get_json.return_value = {'name': 'New Address'}
         mock_create_address.return_value = {'id': 100,
                                             'name': 'New Address'}
         response = AddressList.post()
@@ -41,7 +47,7 @@ class TestApiAddress(unittest.TestCase):
         self.assertEqual(response.json['id'], 100)
         self.assertEqual(response.json['name'], 'New Address')
 
-    @patch('department_app.rest.address.CRUDAddress.get_all_address')
+    @patch('department_app.rest.address.CRUDAddress.get_address_list')
     def test_get_all_address(self, mock_get_all_address):
         """Api should return information about address."""
         mock_get_all_address.return_value = [
