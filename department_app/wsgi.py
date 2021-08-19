7af194085e0bc3e3c6da25539e5188eb1c9d77fb
Ubuntu <awilliamson1889@gmail.com>
2021-08-19 12:23:21 +0300

    Delete db initial from "wsgi.py".

--- department_app/wsgi.py
+++ department_app/wsgi.py
@@ -1,11 +1,5 @@
 """WSGI file"""
 from department_app.app import create_app
-from department_app.database import db
 
 
 app = create_app()
-
-db.init_app(app)
-with app.app_context():
-    db.create_all()
-    db.session.commit()
