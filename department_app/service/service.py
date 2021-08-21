6fe66db8e4e7a44ceb32845c817491b8b036cf2f
Ubuntu <awilliamson1889@gmail.com>
2021-08-21 17:28:00 +0300

    Delete "service.py".

--- department_app/service/service.py
+++
@@ -1,8 +0,0 @@
-"""Service methods"""
-from flask_restful import abort
-
-
-def check_id_format(input_id):
-    """Check ID format"""
-    if not str(input_id).isdigit():
-        abort(404, message="ID must be a number.")
