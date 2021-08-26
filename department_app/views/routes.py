543b317cb1e4d8c9fda516b9e015657f629158a6
Ubuntu <awilliamson1889@gmail.com>
2021-08-26 19:34:07 +0300

    Add views logic.

--- department_app/views/routes.py
+++ department_app/views/routes.py
@@ -1,9 +1,13 @@
 """Routes web-app"""
-from flask import Blueprint, render_template
+from sqlalchemy.exc import IntegrityError
+from flask_restful import abort
+from flask import Blueprint, render_template, request, redirect, url_for
 
+from department_app.service import CRUDDepartment, CRUDAddress, CRUDSkill, CRUDEmployee, CRUDLocation, CRUDPermission
+from department_app.views.forms.forms import DepartmentForm, SkillForm, AddressForm, LocationForm, \
+    PermissionForm, EmployeeForm
 
 frontend = Blueprint('frontend', __name__)
-
 """
 ADD TEMPLATES
 """
@@ -14,34 +18,96 @@ def test():
     return 'Hello World'
 
 
-@frontend.route('/add/address')
+@frontend.route('/add/address', methods=['POST', 'GET'])
 def add_address():
-    return render_template('add_address.html')
+    form = AddressForm(request.form)
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDAddress.create(form.name.data)
+        except IntegrityError:
+            message = "Sorry but this address already exist."
+            return render_template('add_address.html', form=form, message=message)
+        return redirect(url_for('frontend.add_employee'))
+    else:
+        return render_template('add_address.html', form=form)
 
 
-@frontend.route('/add/department')
+@frontend.route('/add/department', methods=['POST', 'GET'])
 def add_department():
-    return render_template('add_department.html')
+    form = DepartmentForm(request.form)
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDDepartment.create(form.name.data, form.date_of_creation.data, form.manager.data)
+        except IntegrityError:
+            message = "Sorry but this department already exist."
+            return render_template('add_department.html', form=form, message=message)
+        return redirect(url_for('frontend.add_employee'))
+    else:
+        return render_template('add_department.html', form=form)
 
 
-@frontend.route('/add/employee')
+@frontend.route('/add/employee', methods=['POST', 'GET'])
 def add_employee():
-    return render_template('add_employee.html')
+    form = EmployeeForm(request.form)
+    form.work_address.choices = [(address['id'], address['name']) for address in CRUDAddress.get_address_list()]
+    form.department.choices = [(dep['department_id'], dep['name']) for dep in CRUDDepartment.get_department_list()]
+    form.location.choices = [(loc['id'], loc['name']) for loc in CRUDLocation.get_location_list()]
+    form.key_skill.choices = [(skill['id'], skill['name']) for skill in CRUDSkill.get_skill_list()]
+    form.permission.choices = [(perm['id'], perm['name']) for perm in CRUDPermission.get_permission_list()]
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDEmployee.create(form.name.data, form.surname.data, form.date_of_birth.data,
+                                form.salary.data, form.email.data, form.phone.data, form.date_of_joining.data,
+                                form.department.data, form.location.data, form.work_address.data, form.key_skill.data,
+                                form.permission.data)
+        except IntegrityError:
+            message = "Sorry but this employee have same fields with another employee."
+            return render_template('add_employee.html', form=form, message=message)
+        return redirect(url_for('frontend.manage_employee_page'))
+    else:
+        return render_template('add_employee.html', form=form)
 
 
-@frontend.route('/add/location')
+@frontend.route('/add/location', methods=['POST', 'GET'])
 def add_location():
-    return render_template('add_location.html')
+    form = LocationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDLocation.create(form.name.data)
+        except IntegrityError:
+            message = "Sorry but this location already exist."
+            return render_template('add_location.html', form=form, message=message)
+        return redirect(url_for('frontend.add_employee'))
+    else:
+        return render_template('add_location.html', form=form)
 
 
-@frontend.route('/add/permission')
+@frontend.route('/add/permission', methods=['POST', 'GET'])
 def add_permission():
-    return render_template('add_permission.html')
+    form = PermissionForm(request.form)
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDPermission.create(form.name.data)
+        except IntegrityError:
+            message = "Sorry but this permission already exist."
+            return render_template('add_permission.html', form=form, message=message)
+        return redirect(url_for('frontend.add_employee'))
+    else:
+        return render_template('add_permission.html', form=form)
 
 
-@frontend.route('/add/skill')
+@frontend.route('/add/skill', methods=['POST', 'GET'])
 def add_skill():
-    return render_template('add_skill.html')
+    form = SkillForm(request.form)
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDSkill.create(form.name.data)
+        except IntegrityError:
+            message = "Sorry but this skill already exist."
+            return render_template('add_skill.html', form=form, message=message)
+        return redirect(url_for('frontend.add_employee'))
+    else:
+        return render_template('add_skill.html', form=form)
 
 
 """
@@ -49,9 +115,13 @@ DELETE TEMPLATES
 """
 
 
-@frontend.route('/delete/employee')
-def delete_employee():
-    return render_template('delete_employee.html')
+@frontend.route('/delete-employee/<string:employee_id>', methods=['POST', 'GET'])
+def delete_employee(employee_id):
+    if request.method == 'POST':
+        CRUDEmployee.delete(employee_id)
+        return redirect(url_for('frontend.manage_employee_page'))
+    else:
+        return render_template('delete_employee.html')
 
 
 """
@@ -59,34 +129,47 @@ PAGES TEMPLATES
 """
 
 
-@frontend.route('/departments')
+@frontend.route('/departments', methods=['POST', 'GET'])
 def departments_page():
-    return render_template('departments.html')
+    departments = CRUDDepartment.get_department_list()
+    if request.method == 'POST':
+        return render_template('departments.html', departments_data=departments)
+    else:
+        return render_template('departments.html', departments_data=departments)
 
 
 @frontend.route('/department/<string:department_id>')
 def department_page(department_id):
-    return render_template('department.html')
+    department = CRUDDepartment.get(department_id)
+    employees = CRUDEmployee.get_employee_list()
+    return render_template('department.html', department_data=department, employees_data=employees)
 
 
 @frontend.route('/employee/<string:employee_id>')
 def employee_page(employee_id):
-    return render_template('profile.html')
+    employee = CRUDEmployee.get(employee_id)
+    return render_template('profile.html', employee_data=employee)
 
 
 @frontend.route('/manage/department')
 def manage_department_page():
-    return render_template('manage_departments.html')
+    departments = CRUDDepartment.get_department_list()
+    return render_template('manage_departments.html', departments_data=departments)
 
 
 @frontend.route('/manage/employee')
 def manage_employee_page():
-    return render_template('manage_employees.html')
+    employees = CRUDEmployee.get_employee_list()
+    return render_template('manage_employees.html', employees_data=employees)
 
 
 @frontend.route('/search/employee')
 def search_employee_page():
-    return render_template('people_search.html')
+    employees = CRUDEmployee.get_employee_list()
+    locations = CRUDLocation.get_location_list()
+    skills = CRUDSkill.get_skill_list()
+    return render_template('people_search.html', employees_data=employees, locations_data=locations,
+                           skills_data=skills)
 
 
 """
@@ -94,14 +177,51 @@ UPDATE TEMPLATES
 """
 
 
-@frontend.route('/update-department/<string:department_id>')
+@frontend.route('/update-department/<string:department_id>', methods=['POST', 'GET'])
 def update_department(department_id):
-    return render_template('update_department.html')
+    form = DepartmentForm(request.form)
+    department = CRUDDepartment.get(department_id)
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDDepartment.update(department_id, form.name.data, form.date_of_creation.data, form.manager.data)
+        except IntegrityError:
+            message = "Sorry but this department already exist."
+            return render_template('add_department.html', form=form, message=message)
+        return redirect(url_for('frontend.manage_department_page'))
+    else:
+        return render_template('update_department.html', department_data=department, form=form)
 
 
-@frontend.route('/update-employee/<string:employee_id>')
+@frontend.route('/update-employee/<string:employee_id>', methods=['POST', 'GET'])
 def update_employee(employee_id):
-    return render_template('update_employee.html')
+    form = EmployeeForm(request.form)
+    form.work_address.choices = [(address['id'], address['name']) for address in CRUDAddress.get_address_list()]
+    form.department.choices = [(dep['department_id'], dep['name']) for dep in CRUDDepartment.get_department_list()]
+    form.location.choices = [(loc['id'], loc['name']) for loc in CRUDLocation.get_location_list()]
+    form.key_skill.choices = [(skill['id'], skill['name']) for skill in CRUDSkill.get_skill_list()]
+    form.permission.choices = [(perm['id'], perm['name']) for perm in CRUDPermission.get_permission_list()]
+
+    departments = CRUDDepartment.get_department_list()
+    addresses = CRUDAddress.get_address_list()
+    locations = CRUDLocation.get_location_list()
+    skills = CRUDSkill.get_skill_list()
+    permissions = CRUDPermission.get_permission_list()
+    employee = CRUDEmployee.get(employee_id)
+
+    if request.method == 'POST' and form.validate():
+        try:
+            CRUDEmployee.update(employee_id, form.name.data, form.surname.data, form.date_of_birth.data,
+                                form.salary.data, form.email.data, form.phone.data, form.date_of_joining.data,
+                                form.department.data, form.location.data, form.work_address.data, form.key_skill.data,
+                                form.permission.data)
+        except IntegrityError:
+            message = "Sorry but this employee have same fields with another employee."
+            return render_template('add_employee.html', form=form, message=message)
+        return redirect(url_for('frontend.manage_employee_page'))
+    else:
+        return render_template('update_employee.html', employee_data=employee, departments_data=departments,
+                               addresses_data=addresses, locations_data=locations, skills_data=skills,
+                               permissions_data=permissions, form=form)
 
 
 """
@@ -113,3 +233,14 @@ I DON'T KNOW FOR WHAT BUT ...
 def logout(employee_id):
     return "LOGOUT"
 
+
+"""
+NOT IMPLEMENTED METHODS
+
+UPDATE ADDRESS
+UPDATE LOCATION
+UPDATE PERMISSION
+UPDATE SKILL
+
+LOGIN PAGE
+"""
