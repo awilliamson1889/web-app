f09e9ca3b6ea0a9a43b88039f31e5e8d74865edf
awilliamson1889 <74731998+awilliamson1889@users.noreply.github.com>
2021-09-05 13:23:21 +0300

    Add some views tests.

--- department_app/tests/views/test_views.py
+++ department_app/tests/views/test_views.py
@@ -44,6 +44,14 @@ class ViewsTestCase(unittest.TestCase):
 
             assert 'Address length must be between 3 and 100 characters' in str(rv.data)
 
+    @patch('department_app.views.routes.CRUDAddress.create', side_effect=IntegrityError('', '', ''))
+    def test_add_address_page_post_already_exist(self, mock_create):
+        """Api should return information about address."""
+        with app.test_request_context():
+            rv = self.app.post('/add/address', data={'name': 'very long name very long name very long name'})
+
+            assert 'Sorry but this address already exist.' in str(rv.data)
+
     def test_add_location_page_get(self):
         with app.test_request_context():
             rv = self.app.get('/add/location')
@@ -73,6 +81,13 @@ class ViewsTestCase(unittest.TestCase):
                                                               'very long name very long name very long name'})
 
             assert 'Location length must be between 3 and 100 characters' in str(rv.data)
+
+    @patch('department_app.views.routes.CRUDLocation.create', side_effect=IntegrityError('', '', ''))
+    def test_add_location_page_post_already_exist(self, mock_create):
+        with app.test_request_context():
+            rv = self.app.post('/add/location', data={'name': 'very long name very long name very long name'})
+
+            assert 'Sorry but this location already exist.' in str(rv.data)
 
     def test_add_permission_page_get(self):
         with app.test_request_context():
@@ -104,6 +119,13 @@ class ViewsTestCase(unittest.TestCase):
 
             assert 'Permission length must be between 3 and 50 characters' in str(rv.data)
 
+    @patch('department_app.views.routes.CRUDPermission.create', side_effect=IntegrityError('', '', ''))
+    def test_add_permission_page_post_already_exist(self, mock_create):
+        with app.test_request_context():
+            rv = self.app.post('/add/permission', data={'name': 'very long name very long name very long name'})
+
+            assert 'Sorry but this permission already exist.' in str(rv.data)
+
     def test_add_skill_page_get(self):
         with app.test_request_context():
             rv = self.app.get('/add/skill')
@@ -133,3 +155,49 @@ class ViewsTestCase(unittest.TestCase):
                                                            'very long name very long name very long name'})
 
             assert 'Skill length must be between 3 and 50 characters' in str(rv.data)
+
+    @patch('department_app.views.routes.CRUDSkill.create', side_effect=IntegrityError('', '', ''))
+    def test_add_skill_page_post_already_exist(self, mock_create):
+        with app.test_request_context():
+            rv = self.app.post('/add/skill', data={'name': 'very long name very long name very long name'})
+
+            assert 'Sorry but this skill already exist.' in str(rv.data)
+
+    def test_add_department_page_get(self):
+        with app.test_request_context():
+            rv = self.app.get('/add/department')
+            self.assertEqual(rv.request.path, '/add/department')
+            self.assertEqual(rv.request.method, 'GET')
+            self.assertEqual(rv.status_code, 200)
+
+            assert '<title>Add department</title>' in str(rv.data)
+
+    def test_add_department_page_post_empty_name_field(self):
+        with app.test_request_context():
+            rv = self.app.post('/add/department', data={'manager': 'Manager Managerov',
+                                                        'date_of_creation': '1999-01-01'})
+            self.assertEqual(rv.request.path, '/add/department')
+            self.assertEqual(rv.request.method, 'POST')
+            self.assertEqual(rv.status_code, 200)
+
+            assert 'This field is required' in str(rv.data)
+            assert '<title>Add department</title>' in str(rv.data)
+
+    def test_add_employee_page_get(self):
+        with app.test_request_context():
+            rv = self.app.get('/add/employee')
+            self.assertEqual(rv.request.path, '/add/employee')
+            self.assertEqual(rv.request.method, 'GET')
+            self.assertEqual(rv.status_code, 200)
+
+            assert '<title>Add employee</title>' in str(rv.data)
+
+    def test_add_employee_page_post_empty_fields(self):
+        with app.test_request_context():
+            rv = self.app.post('/add/employee')
+            self.assertEqual(rv.request.path, '/add/employee')
+            self.assertEqual(rv.request.method, 'POST')
+            self.assertEqual(rv.status_code, 200)
+
+            assert 'This field is required' in str(rv.data)
+            assert '<title>Add employee</title>' in str(rv.data)
